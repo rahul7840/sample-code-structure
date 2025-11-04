774403762877f7851b2bfe9720b3d59fcb704a31
rahul-kalasuva-shivalik <rahul@shivalikgroup.com>
2025-11-04 18:51:57 +0530

    changes

--- deploy/postScript.sh
+++ deploy/postScript.sh
@@ -6,13 +6,14 @@ aws ecr get-login-password --region ap-south-1 \
   | docker login --username AWS --password-stdin 208116833517.dkr.ecr.ap-south-1.amazonaws.com
 
 # Stop old container
-docker stop user-service || true
-docker rm user-service || true
+docker stop sample-server || true
+docker rm sample-server || true
 
 # Pull latest image
-docker pull 208116833517.dkr.ecr.ap-south-1.amazonaws.com/user-service:latest
+docker pull 208116833517.dkr.ecr.ap-south-1.amazonaws.com/sample-server:latest
+
 
 # Run new container
-docker run -d --name user-service -p 11001:11001 208116833517.dkr.ecr.ap-south-1.amazonaws.com/user-service:latest
+docker run -d --name sample-server -p 11001:11001 208116833517.dkr.ecr.ap-south-1.amazonaws.com/sample-server:latest
 
 echo "✅ Deployment completed!"
