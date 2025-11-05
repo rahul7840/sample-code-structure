4310a1991385b4c25c0a7b43569fe374c3a4b4a3
rahul-kalasuva-shivalik <rahul@shivalikgroup.com>
2025-11-05 12:03:48 +0530

    changes

--- deploy/postScript.sh
+++ deploy/postScript.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
+exec > /tmp/postdeploy.log 2>&1
+set -x
+
 echo "🚀 Starting deployment..."
 
 # Ensure Docker is running
-systemctl start docker || true
+sudo systemctl start docker || true
 
 # Stop old container if running
-docker stop sample-server || true
-docker rm sample-server || true
+sudo docker stop sample-server || true
+sudo docker rm sample-server || true
 
 # Pull latest image from ECR (auth handled by ECR Credential Helper)
 echo "📦 Pulling latest image..."
-docker pull 208116833517.dkr.ecr.ap-south-1.amazonaws.com/sample-server:latest
+sudo docker pull 208116833517.dkr.ecr.ap-south-1.amazonaws.com/sample-server:latest
 
 # Run new container
 echo "🚀 Starting new container..."
-docker run -d --name sample-server -p 11001:11001 \
+sudo docker run -d --restart unless-stopped --name sample-server -p 11001:11001 \
   208116833517.dkr.ecr.ap-south-1.amazonaws.com/sample-server:latest
 
 echo "✅ Deployment completed successfully!"
