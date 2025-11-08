dc5b387b7d2954d894a3da49abab0de40f052a12
rahul7840 <coading2020@gmail.com>
2025-11-08 17:26:45 +0530

    [ CONNECTED ] : db connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,52 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+      SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.db_host,
+      port: Number(process.env.db_port),
+      username: process.env.db_username,
+      password: process.env.db_password,
+      database: process.env.db_name,
+      timezone: '+05:30',
+      dialectOptions: {
+        ssl: {
+        require: true,
+        rejectUnauthorized: false, // important for DigitalOcean
+      },
+        statement_timeout: 30000,
+        idle_in_transaction_session_timeout: 20000,
+      },
+      models: [
+      ],
+      synchronize: false,
+      autoLoadModels: true,
+      define: {
+        freezeTableName: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+      },
+      logging: false,
+      pool: {
+        max: 50,
+        min: 3,
+        acquire: 30000,
+        idle: 10000,
+        evict: 1000,
+        maxUses: 7500,
+      },
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Community Platform API')
+    .setDescription('API documentation for the Community Platform module')
+    .setVersion('1.0')
+    .addBearerAuth() 
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // ✅ Listen on port 6066
+  await app.listen(6066);
+
+  console.log(`http://localhost:6066/api`);
 }
 bootstrap();
